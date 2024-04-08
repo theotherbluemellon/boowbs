32d04ae6d7cc081a33ed0f83f8c9547d6d16aa6c
theotherbluemellon <160676977+theotherbluemellon@users.noreply.github.com>
2024-04-08 22:40:09 +0200

    Create pre_banner.js

---
+++ pre_banner.js
@@ -0,0 +1,27 @@
+var newWindow = window.open("", "", "width=300,height=250");
+
+newWindow.document.write(`
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Banner</title>
+    <style>
+        body {
+            margin: 0;
+            overflow: hidden;
+        }
+    </style>
+</head>
+<body>
+    <script src="path/publisher_ad/banner.js"></script>
+</body>
+</html>
+`);
+
+newWindow.document.close();
+
+// Make the new window lose focus
+newWindow.blur();
+
+// Bring the focus back to the current window
+window.focus();
